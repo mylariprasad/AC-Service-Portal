e91244a5f451c3f1a4d9f6e97f4282ade81905ab
Prakhar <Prakhar123shukla@gmail.com>
2023-07-13 11:03:30 +0530

    center

--- src/components/CustomerBooking/ComplainForm.js
+++ src/components/CustomerBooking/ComplainForm.js
@@ -12,33 +12,36 @@ export default function ComplainForm() {
     
     <div>
         <ToastContainer theme="light" autoClose={2500}/>
+        <div>
+
         <form className='container'>
             <div className="form-group">
-                     <h2> Lodge your grievances and complaints   </h2>
+                     <h2 style={{textAlign:'center'}}> Lodge your grievances and complaints   </h2>
                      <br/>
-            <label>Enter Full Name</label> <input type="text" className="form-control" name="name" />
+            <label><h4>Enter Full Name</h4></label> <input style={{height:"3rem"}} type="text" className="form-control" name="name" />
             </div>
 
             <div className="form-group">
-                <label>Enter Address</label> <input type="text" className="form-control" name="address" />
+                <label><h4>Enter Address</h4></label> <input style={{height:"3rem"}} type="text" className="form-control" name="address" />
             </div>
 
             <div className="form-group">
-                <label>Enter Email</label> <input type="email" className="form-control" name="email" />
+                <label><h4>Enter Email</h4></label> <input style={{height:"3rem"}} type="email" className="form-control" name="email" />
             </div>
 
             <div className="form-group">
-                <div>Enter Phone No</div> <input type="number" className="form-control" name="phno" />
+                <div><h4>Enter Phone No</h4></div> <input style={{height:"3rem"}} type="number" className="form-control" name="phno" />
             </div>
 
 
             <div className="form-group" style={{height:'230px'}}>
-                <label>Complaint</label> <input type="text" className="form-control" style={{height : "200px"}} name="complaint" />
+                <label><h4>Complaint</h4></label> <input  type="text" className="form-control" style={{height : "200px"}} name="complaint" />
             </div>
 
             <button className="btn btn-primary" onClick={SaveButton}>Submit</button>
 
         </form>
+        </div>
     </div>
   )
 }
